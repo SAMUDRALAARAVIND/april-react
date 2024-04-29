f062fcff566227c70a6d0a4a725bddaddf9c2783
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2024-04-29 23:41:32 +0530

    redux introduction part 2

--- src/network/endpoints.js
+++
@@ -1,8 +0,0 @@
-// REST Endpoints 
-const baseUrl = "https://node-auth-jwt-w78c.onrender.com";
-const endpoints = {
-    login: `${baseUrl}/auth/login`,
-    signup: `${baseUrl}/auth/signup`
-}
-
-export default endpoints
